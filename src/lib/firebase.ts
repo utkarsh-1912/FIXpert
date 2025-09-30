b7bb563987c153775411d9bd54ceabcbe3b8af75
Utkarsh Gupta <utkarsh.19.12.01@gmail.com>
2025-09-30 06:36:18 +0000

    api key 1 AIzaSyAgnLS2jKHKlYUUbRCadpIzVTue0cFmhyo

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -4,7 +4,7 @@ import { initializeApp, getApps, getApp, FirebaseApp } from 'firebase/app';
 const firebaseConfig = {
   projectId: 'studio-4305945131-b5e3e',
   appId: '1:538411774967:web:cc1531f5ac1c2d300d74f2',
-  apiKey: 'AIzaSyCgIzj_07NSPAq2euBvX968oJZodMICIE4',
+  apiKey: 'AIzaSyAgnLS2jKHKlYUUbRCadpIzVTue0cFmhyo',
   authDomain: 'studio-4305945131-b5e3e.firebaseapp.com',
   measurementId: '',
   messagingSenderId: '538411774967',
