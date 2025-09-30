d036a4ce5a5cf4c2599de71466edc76ddae3be16
Utkarsh Gupta <utkarsh.19.12.01@gmail.com>
2025-09-30 19:19:40 +0000

    show actual price

--- src/app/(app)/symbol-search/[symbol]/page.tsx
+++ src/app/(app)/symbol-search/[symbol]/page.tsx
@@ -144,7 +144,7 @@ export default function SymbolDashboardPage() {
               <ChartContainer config={chartConfig} className="h-full w-full">
                 {chartData && chartData.length > 0 ? (
                   <ResponsiveContainer width="100%" height="100%">
-                    <AreaChart data={chartData} margin={{ top: 10, right: 10, left: -10, bottom: 0 }}>
+                    <AreaChart data={chartData} margin={{ top: 10, right: 10, left: 0, bottom: 0 }}>
                       <defs>
                         <linearGradient id="chart-gradient" x1="0" y1="0" x2="0" y2="1">
                           <stop offset="5%" stopColor={chartColor} stopOpacity={0.8}/>
