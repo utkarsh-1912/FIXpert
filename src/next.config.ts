04640ad6fec6bca4fb7272250974e943b890cc8a
Utkarsh Gupta <utkarsh.19.12.01@gmail.com>
2025-09-30 18:33:48 +0000

    Try fixing this error: `Runtime Error: Error: ./node_modules/dagre-d3-es

---
+++ src/next.config.ts
@@ -0,0 +1,36 @@
+import type {NextConfig} from 'next';
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'picsum.photos',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+  serverComponentsExternalPackages: ['dagre-d3-es'],
+};
+
+export default nextConfig;
