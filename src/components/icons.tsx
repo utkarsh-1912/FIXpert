5f70b19a0c5c1948c070a0d1f23fc724f14daca1
Utkarsh Gupta <utkarsh.19.12.01@gmail.com>
2025-09-30 11:02:17 +0000

    update logo

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -1,16 +1,13 @@
 export const FixpertIcon = (props: React.SVGProps<SVGSVGElement>) => (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      {...props}
-    >
-      <path d="M12 3v18" />
-      <path d="M3 3h9" />
-      <path d="M3 9h9" />
-    </svg>
-  );
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 100 68"
+    fill="currentColor"
+    {...props}
+  >
+    <path d="M4.23 0H14.1L14.03 14.54H25.02V23.41H14.03V42.06H32.6V50.93H14.03V68H4.16V0.07L4.23 0Z" />
+    <path d="M37.94 14.54H47.88V68H37.94V14.54Z" />
+    <path d="M13.96 14.54H4.23V23.41H13.96V14.54Z" />
+    <path d="M60.67 14.54L74.21 34.35L87.75 14.54H100L80.12 41.2L100 68H87.82L74.21 48.01L60.6 68H48.42L68.3 41.2L48.42 14.54H60.67Z" />
+  </svg>
+);
