c3b2049807feae7bd62449b7437b8b5f669b9b46
Utkarsh Gupta <utkarsh.19.12.01@gmail.com>
2025-09-30 06:39:29 +0000

    File changes

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -4,18 +4,19 @@ import { initializeApp, getApps, getApp, FirebaseApp } from 'firebase/app';
 const firebaseConfig = {
   projectId: 'studio-4305945131-b5e3e',
   appId: '1:538411774967:web:cc1531f5ac1c2d300d74f2',
-  apiKey: 'AIzaSyAgnLS2jKHKlYUUbRCadpIzVTue0cFmhyo',
+  apiKey: 'AIzaSyCgIzj_07NSPAq2euBvX968oJZodMICIE4',
   authDomain: 'studio-4305945131-b5e3e.firebaseapp.com',
   measurementId: '',
   messagingSenderId: '538411774967',
 };
 
-// Initialize Firebase
+
 let app: FirebaseApp;
-if (getApps().length === 0) {
+if (!getApps().length) {
   app = initializeApp(firebaseConfig);
 } else {
   app = getApp();
 }
+
 
 export { app };
